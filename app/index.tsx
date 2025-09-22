ae981fa7868f09712cad211af9221e86a0f74fdf
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-09-22 11:50:22 -0300

    exclusao das paginas

---
+++ app/index.tsx
@@ -0,0 +1,38 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet, View, Text } from "react-native";
+
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 16,
+      }}
+    >
+      <Text>Seja bem vido BarbAPP</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
