be6e6b7bb4ff7b0b38b6d0ece85deb9674f4ed5b
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-10-19 19:33:42 -0300

    implementaçao do serviços pagina de agenda

--- src/app/(tabs)/Agenda.tsx
+++ src/app/(tabs)/Agenda.tsx
@@ -1,10 +1,21 @@
+import { Picker } from "@react-native-picker/picker";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Agendamento() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Serviços</Text>
-      <Text style={styles.barbeiro}>Barbeiro</Text>
+      <Picker>
+        <Picker.Item label="Corte de Cabelo" value="corte_de_cabelo" />
+        <Picker.Item label="Barba" value="barba" />
+        <Picker.Item label="Corte e Barba" value="corte_e_barba" />
+      </Picker>
+
+      <View>
+        <Text style={styles.barbeiro}>Barbeiros</Text>
+      </View>
+
       <View>
         <Text style={styles.date}>Data e Hora</Text>
       </View>
@@ -14,7 +25,9 @@ export default function Agendamento() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 10, backgroundColor: "#221610" },
+
   text: { fontSize: 24, color: "#FFF" },
-  barbeiro: { fontSize: 18, color: "#FFF", marginTop: 10 },
-  date: { fontSize: 18, color: "#FFF", marginTop: 10 },
+
+  barbeiro: { fontSize: 24, color: "#FFF", marginTop: 10 },
+  date: { fontSize: 24, color: "#FFF", marginTop: 10 },
 });
